9b40c5582ab4968d946d762ea936c2313bc78ca0
Colin <colin.diesh@gmail.com>
2021-11-03 09:47:18 -0400

    Update snaps

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,4 +6,6 @@ test("gather", async () => {
     "out.fai",
     fs.createReadStream(require.resolve("./volvox.fa"))
   );
+  const r = fs.readFileSync("out.fai", "utf8");
+  expect(r).toMatchSnapshot();
 });
