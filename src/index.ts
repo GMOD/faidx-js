43ab5852ce64972836d1be3e45c696aca605b727
Colin <colin.diesh@gmail.com>
2024-03-05 17:15:46 -0500

    Add eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,85 @@
-import fs from "fs";
-import split2 from "split2";
-import pump from "pump";
-import { Writable, Readable, Transform } from "stream";
+import split2 from 'split2'
+import pump from 'pump'
+import { Writable, Readable, Transform } from 'stream'
 
 // creates an FAI file from a FASTA file streaming in
 class FastaIndexTransform extends Transform {
-  foundAny = false;
-  possibleBadLine = undefined as [number, string] | undefined;
-  refName: string | undefined;
-  currOffset = 0;
-  refSeqLen = 0;
-  lineBytes = 0;
-  lineBases = 0;
-  refOffset = 0;
-  lineNum = 0;
+  foundAny = false
+  possibleBadLine = undefined as [number, string] | undefined
+  refName: string | undefined
+  currOffset = 0
+  refSeqLen = 0
+  lineBytes = 0
+  lineBases = 0
+  refOffset = 0
+  lineNum = 0
 
   _transform(chunk: Buffer, encoding: unknown, done: (error?: Error) => void) {
-    const line = chunk.toString();
+    const line = chunk.toString()
     // line length in bytes including the \n that we split on
-    const currentLineBytes = chunk.length + 1;
+    const currentLineBytes = chunk.length + 1
     // chop off \r if exists
-    const currentLineBases = line.trim().length;
-    if (line[0] === ">") {
-      this.foundAny = true;
+    const currentLineBases = line.trim().length
+    if (line.startsWith('>')) {
+      this.foundAny = true
       if (
         this.possibleBadLine &&
         this.possibleBadLine[0] !== this.lineNum - 1
       ) {
-        done(new Error(this.possibleBadLine[1]));
-        return;
+        done(new Error(this.possibleBadLine[1]))
+        return
       }
       if (this.lineNum > 0) {
         this.push(
-          `${this.refName}\t${this.refSeqLen}\t${this.refOffset}\t${this.lineBases}\t${this.lineBytes}\n`
-        );
+          `${this.refName}\t${this.refSeqLen}\t${this.refOffset}\t${this.lineBases}\t${this.lineBytes}\n`,
+        )
       }
       // reset
-      this.lineBytes = 0;
-      this.refSeqLen = 0;
-      this.lineBases = 0;
-      this.refName = line.trim().slice(1).split(/\s+/)[0];
-      this.currOffset += currentLineBytes;
-      this.refOffset = this.currOffset;
-      this.possibleBadLine = undefined;
+      this.lineBytes = 0
+      this.refSeqLen = 0
+      this.lineBases = 0
+      this.refName = line.trim().slice(1).split(/\s+/)[0]
+      this.currOffset += currentLineBytes
+      this.refOffset = this.currOffset
+      this.possibleBadLine = undefined
     } else {
       if (this.lineBases && currentLineBases !== this.lineBases) {
         this.possibleBadLine = [
           this.lineNum,
           `Not all lines in file have same width, please check your FASTA file line ${this.lineNum}`,
-        ];
+        ]
       }
-      this.lineBytes = currentLineBytes;
-      this.lineBases = currentLineBases;
-      this.currOffset += currentLineBytes;
-      this.refSeqLen += currentLineBases;
+      this.lineBytes = currentLineBytes
+      this.lineBases = currentLineBases
+      this.currOffset += currentLineBytes
+      this.refSeqLen += currentLineBases
     }
 
-    this.lineNum++;
-    done();
+    this.lineNum++
+    done()
   }
 
   _flush(done: (error?: Error) => void) {
     if (!this.foundAny) {
       done(
         new Error(
-          "No sequences found in file. Ensure that this is a valid FASTA file"
-        )
-      );
+          'No sequences found in file. Ensure that this is a valid FASTA file',
+        ),
+      )
     } else {
       if (this.lineNum > 0) {
         this.push(
-          `${this.refName}\t${this.refSeqLen}\t${this.refOffset}\t${this.lineBases}\t${this.lineBytes}\n`
-        );
+          `${this.refName}\t${this.refSeqLen}\t${this.refOffset}\t${this.lineBases}\t${this.lineBytes}\n`,
+        )
       }
-      done();
+      done()
     }
   }
 }
 
 export async function generateFastaIndex(
   fileWriteStream: Writable,
-  fileDataStream: Readable
+  fileDataStream: Readable,
 ) {
   return new Promise((resolve, reject) => {
     pump(
@@ -90,11 +89,11 @@ export async function generateFastaIndex(
       fileWriteStream,
       function (err) {
         if (err) {
-          reject(err);
+          reject(err)
         } else {
-          resolve("success");
+          resolve('success')
         }
-      }
-    );
-  });
+      },
+    )
+  })
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,13 @@
-import fs from "fs";
-import { generateFastaIndex } from "../src";
+import fs from 'fs'
+import { generateFastaIndex } from '../src'
 
-test("gather", async () => {
+test('gather', async () => {
   await generateFastaIndex(
-    fs.createWriteStream("out.fai"),
-    fs.createReadStream(require.resolve("./volvox.fa"))
-  );
+    fs.createWriteStream('test/out.fai'),
+    fs.createReadStream(require.resolve('./volvox.fa')),
+  )
 
-  const r = fs.readFileSync("out.fai", "utf8");
-  expect(r).toMatchSnapshot();
-});
+  const writtenOutput = fs.readFileSync('test/out.fai', 'utf8')
+  const samtoolsFaidxOutput = fs.readFileSync('test/volvox.fa.fai', 'utf8')
+  expect(writtenOutput).toMatch(samtoolsFaidxOutput)
+})
